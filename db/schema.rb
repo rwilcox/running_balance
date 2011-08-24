e88723cddfdb8ab0c867f8ad105db3fd1f8ea384
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-08-23 21:22:52 -0700

    Updated schema with active_on transaction field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110818232851) do
+ActiveRecord::Schema.define(:version => 20110823023630) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110818232851) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "active_on"
   end
 
   create_table "users", :force => true do |t|
