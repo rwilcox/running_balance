22b6d93c64f13f574673502ac68c50fd6246a2fb
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-12-23 15:38:03 -0500

    whitespace changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     # if the user hasn't set it, do some browser sniffing
     mobile_browsers = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo",
       "plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;",
-      "windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", 
+      "windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone",
       "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
 
     agent = (request.headers["HTTP_USER_AGENT"] || "DEFAULT").downcase
@@ -20,7 +20,6 @@ module ApplicationHelper
   end
 
   def link_to_appropriate( page_name, our_options={}, link_to_options={} )
-
     page_path = ""
     if our_options[:desktop] || our_options[:mobile]
       page_path = ( use_mobile_browsing?() ? our_options[:mobile] : our_options[:desktop] )
