37e016b6bd05b125ffef7572b2d43f8231246d2e
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-12-23 18:55:40 -0500

    added appropriate_layout method, which will take mobile into consideration, in addition to an action's request to just be plain (aka: jQuery mobile pages)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   extend DelegatePresenter::ApplicationController
+  include ApplicationHelper
 
   protect_from_forgery
 
@@ -17,5 +18,30 @@ protected
 
   def store_target_location
     session[:return_to] = request.path
+  end
+
+
+=begin
+  Pick the appropriate layout for this context based on if the user is mobile or desktop
+
+  Options you can pass in to change the "appropriate" layout:
+
+    * [:just_content] -> uses the minimal layout (meant for jQuery mobile pages
+=end
+  def appropriate_layout(*args)
+    # TODO: Ideally I want to use the name of the current controller to look up and
+    # see if we have a layout specific to that controller. For now we have a flat layout
+
+    options = args.extract_options!
+
+    layout_name = "application"
+
+    if use_mobile_browsing?
+      if options[:just_content] == true
+        layout_name = "just_content"
+      end
+    end
+
+    layout_name
   end
 end
