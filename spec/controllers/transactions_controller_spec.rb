f607edfed7ccac23a542a723cb6be724e814736c
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-12-25 10:01:24 -0500

    whitespace changes

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -7,7 +7,7 @@ describe TransactionsController do
   # Transaction. As you add validations to Transaction, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {:account_id => @account.id}
+    {:account_id => @account.id, description: "TEST TRANSACTION", amount: 42}
   end
 
   describe "as an unauthorized user, it redirects me to the signup page" do
@@ -19,7 +19,7 @@ describe TransactionsController do
       get :index, {account_id: @account.id}
       response.should redirect_to( new_user_session_path )
     end
-  
+
     it "when I try to access a created transaction" do
       transaction = Transaction.create! valid_attributes
       get :show, :id => transaction.id.to_s, :account_id => @account.id
@@ -49,7 +49,7 @@ describe TransactionsController do
       expect {
         delete :destroy, :id => transaction.id.to_s, :account_id => @account.id
       }.to_not change(Transaction, :count)
-      
+
       response.should redirect_to( new_user_session_path )
     end
   end
@@ -65,7 +65,7 @@ describe TransactionsController do
     it "not access the the transactions index" do
       expect {
         get :index, {account_id: @account.id}
-      }.to raise_error(ActiveRecord::RecordNotFound)      
+      }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
     it "not access a transaction" do
@@ -111,7 +111,7 @@ describe TransactionsController do
 
       sign_in @current_user
     end
-    
+
     describe "GET index" do
       it "assigns all transactions as @transactions" do
         transaction = Transaction.create! valid_attributes
@@ -119,7 +119,7 @@ describe TransactionsController do
         assigns(:transactions).should eq([transaction])
       end
     end
-  
+
     describe "GET show" do
       it "assigns the requested transaction as @transaction" do
         transaction = Transaction.create! valid_attributes
@@ -127,14 +127,14 @@ describe TransactionsController do
         assigns(:transaction).should eq(transaction)
       end
     end
-  
+
     describe "GET new" do
       it "assigns a new transaction as @transaction" do
         get :new, {account_id:  @account.id}
         assigns(:transaction).should be_a_new(Transaction)
       end
     end
-  
+
     describe "GET edit" do
       it "assigns the requested transaction as @transaction" do
         transaction = Transaction.create! valid_attributes
@@ -142,7 +142,7 @@ describe TransactionsController do
         assigns(:transaction).should eq(transaction)
       end
     end
-  
+
     describe "POST create" do
       describe "with valid params" do
         it "creates a new Transaction" do
@@ -150,19 +150,19 @@ describe TransactionsController do
             post :create, {transaction: valid_attributes, account_id: @account.id}
           }.to change(Transaction, :count).by(1)
         end
-  
+
         it "assigns a newly created transaction as @transaction" do
           post :create, {transaction: valid_attributes, account_id: @account.id}
           assigns(:transaction).should be_a(Transaction)
           assigns(:transaction).should be_persisted
         end
-  
+
         it "redirects to the created transaction" do
           post :create, {transaction: valid_attributes, account_id: @account}
           response.should redirect_to( account_transaction_path(@account, Transaction.last) )
         end
       end
-  
+
       describe "with invalid params" do
         it "assigns a newly created but unsaved transaction as @transaction" do
           # Trigger the behavior that occurs when invalid params are submitted
@@ -170,7 +170,7 @@ describe TransactionsController do
           post :create, {transaction: {}, account_id: @account.id}
           assigns(:transaction).should be_a_new(Transaction)
         end
-  
+
         it "re-renders the 'new' template" do
           # Trigger the behavior that occurs when invalid params are submitted
           Transaction.any_instance.stub(:save).and_return(false)
@@ -179,7 +179,7 @@ describe TransactionsController do
         end
       end
     end
-  
+
     describe "PUT update" do
       describe "with valid params" do
         it "updates the requested transaction" do
@@ -187,20 +187,20 @@ describe TransactionsController do
           Transaction.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
           put :update, :id => transaction.id, :transaction => {'these' => 'params'}, :account_id => @account.id
         end
-  
+
         it "assigns the requested transaction as @transaction" do
           transaction = Transaction.create! valid_attributes
           put :update, :id => transaction.id, :transaction => valid_attributes, :account_id => @account.id
           assigns(:transaction).should eq(transaction)
         end
-  
+
         it "redirects to the transaction" do
           transaction = Transaction.create! valid_attributes
           put :update, :id => transaction.id, :transaction => valid_attributes, :account_id => @account.id
           response.should redirect_to( account_transaction_path(@account, transaction) )
         end
       end
-  
+
       describe "with invalid params" do
         it "assigns the transaction as @transaction" do
           transaction = Transaction.create! valid_attributes
@@ -209,7 +209,7 @@ describe TransactionsController do
           put :update, :id => transaction.id.to_s, :transaction => {}, :account_id => @account.id
           assigns(:transaction).should eq(transaction)
         end
-  
+
         it "re-renders the 'edit' template" do
           transaction = Transaction.create! valid_attributes
           # Trigger the behavior that occurs when invalid params are submitted
@@ -219,7 +219,7 @@ describe TransactionsController do
         end
       end
     end
-  
+
     describe "DELETE destroy" do
       it "destroys the requested transaction" do
         transaction = Transaction.create! valid_attributes
@@ -227,7 +227,7 @@ describe TransactionsController do
           delete :destroy, :id => transaction.id.to_s, :account_id => @account.id
         }.to change(Transaction, :count).by(-1)
       end
-  
+
       it "redirects to the transactions list" do
         transaction = Transaction.create! valid_attributes
         delete :destroy, :id => transaction.id.to_s, :account_id => @account.id
